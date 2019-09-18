2eb30dda4ab5a5a76bf83391838269481da901f5
AaruniAggarwal <AaruniAggarwal@DESKTOP-E24IGJP.in.ibm.com>
2019-09-18 15:18:11 +0530

    commit 2

---
+++ labs/ThreadCallable/src/CallableServiceExample.java
@@ -0,0 +1,62 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+public class CallableServiceExample {
+	public class MyCallable implements Callable<String> {
+		@Override
+		public String call() throws Exception {
+			System.out.println(Thread.currentThread().getName() + " -->Start  thread");
+			
+			int sum=0;
+			try {
+				Thread.sleep(1000);
+				for(int i=0;i<100;i++)
+				{
+					sum=sum+i;
+					System.out.println(sum);
+				}
+			} catch (InterruptedException e) {
+				//throw new MyException();
+			}
+			
+			return "Hii";
+		}
+	}
+
+	public static void main(String[] args) {
+		CallableServiceExample eg = new CallableServiceExample();
+		
+
+		Callable<String> c1 = eg.new MyCallable();
+				
+
+		
+		ExecutorService e = Executors.newFixedThreadPool(1);
+
+		Future<String> f = e.submit(c1);
+
+			while(!f.isDone()) {
+				System.out.println(Thread.currentThread().getName() + " -->Wait");
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+			String s = null;
+			
+			try {
+				s = f.get();
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			} catch (ExecutionException e1) {
+				e1.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + " " +s);
+	}
+
+}
